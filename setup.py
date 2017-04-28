800549fde36160373e56bd0c3fdacd3d00f8cafe
Pooja Shah <poojashah.cse@gmail.com>
2017-04-28 13:27:52 +0530

    setup chnages

--- setup.py
+++ setup.py
@@ -1,18 +1,32 @@
 from setuptools import setup, find_packages
 
+# Get the long description from the README file
+with open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+
+with open('VERSION', encoding='utf-8') as f:
+    package_version = f.read()
+
+with open('requirements.txt', encoding='utf-8') as f:
+    dependencies = []
+    for line in f:
+        line = line.strip()  # or someother preprocessing
+        dependencies.append(line)
+
 setup(
     name='alice-pooja',
-    version='1.0',
-    py_modules= '',
+    version=package_version,
+    license='MIT',
     author='Pooja Shah',
     author_email='writeback2pooja@gmail.com',
-    #long_description=__doc__,
-    long_description=open('README').read(),
-    packages=find_packages(),
+    long_description=long_description,
+    packages=find_packages(exclude=['build', 'dist']),
+    url='https://github.com/moengage/alice',
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        'Flask==0.13.dev0',
-        'slacker==0.9.24',
-        'python-jenkins==0.4.13'])
+    install_requires=dependencies
+   )
+
+
+
 
