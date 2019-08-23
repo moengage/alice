2f12056d1c01da015492834f0f5b8ab9b3c8a5b7
paras <parasjain@moengage.com>
2019-08-23 14:46:43 +0530

    Removed redundant data

--- alice/helper/common_utils.py
+++ alice/helper/common_utils.py
@@ -6,12 +6,7 @@ import json
 from alice.helper.api_manager import ApiManager
 from alice.helper.constants import API_GITHUB_MEMBERS_LIST, API_GITHUB_USERS, SLACK_USER_LIST
 from alice.helper.file_utils import get_dict_from_config_file
-
-from alice.config.config_provider import ConfigProvider
-
-git_mappings = {
-    "paras" : "UL91SP77H"
-}
+git_mappings = {}
 slack_mappings = {}
 
 
