188e20429d11e0aece35d1b9905de042f4355d7c
paras <parasjain@moengage.com>
2019-11-04 16:36:56 +0530

    Added fix for other branches

--- alice/main/runner.py
+++ alice/main/runner.py
@@ -8,7 +8,6 @@ from alice.checker_impl import CheckImpl
 from alice.helper.jenkins_helper import JenkinsHelper
 
 
-
 class RunChecks(object):
     def execute_check(self, ci, check):
         LOG.debug("************* Starting check=%s *****************" % check)
@@ -40,6 +39,14 @@ class RunChecks(object):
                     if 'invalid_auth' not in str(e):
                         raise Exception(str(e) + ISSUE_FOUND.format(issue_link=ISSUE_LINK))
             return response
+        else:
+            JenkinsHelper(ci.pr).change_status(ci.pr.statuses_url, "success", context="shield-syntax-validator-python",
+                                       description="Skipped Testing as not sensitive branch",
+                                       details_link="")
+            JenkinsHelper(ci.pr).change_status(ci.pr.statuses_url, "success", context="shield-unit-test-python",
+                                       description="Skipped Testing as not sensitive branch",
+                                       details_link="")
+
         LOG.info("skipped because '%s' is not sensitive branch" % ci.pr.base_branch)
         return {"msg": "skipped because '%s' is not sensitive branch" % ci.pr.base_branch}
 
