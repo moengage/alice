d6d6d3d4a49ba741f3ad3e0747a7063e4a9c9c74
paras <parasjain@moengage.com>
2020-08-19 11:20:15 +0530

    changed context

--- alice/main/actor.py
+++ alice/main/actor.py
@@ -1192,7 +1192,6 @@ class Actor(Base):
             headers = {"Authorization": "token " + self.github.GITHUB_TOKEN}
             response = ApiManager.get(url_with_page, headers)
             res = json.loads(response["content"])
-            #print("chunky",response, res, url_with_page)
             if not res or (isinstance(res, dict) and "limit exceeded" in res.get("message")):
                 print(res)
                 break
@@ -1389,12 +1388,14 @@ class Actor(Base):
                         if repo == moengage_repo:
                             if not is_required_files_present: # checks for version files
                                 print("Required files are not present")
-                                self.jenkins.change_status(self.pr.statuses_url, "failure", context='Block-PR: File',
-                                                           description="Version , changelog files not found",
+                                self.jenkins.change_status(self.pr.statuses_url, "failure",
+                                                           context='shield-release-checklist', description=
+                                                           "Missing update on VERSION | CHANGELOG update file(s)",
                                                            details_link="")
                             else:
-                                self.jenkins.change_status(self.pr.statuses_url, "success", context='Block-PR: File',
-                                                           description="Version and changelog files are present",
+                                self.jenkins.change_status(self.pr.statuses_url, "success",
+                                                           context='shield-release-checklist', description=
+                                                           "VERSION | CHANGELOG update are good",
                                                            details_link="")
 
                         if repo in JAVA_REPO:
