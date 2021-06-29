2e3fb9d9d0e9635495225aa7fb411f10e6ab149b
paras <parasjain@moengage.com>
2021-06-29 20:02:56 +0530

    commented slack alert

--- alice/helper/slack_helper.py
+++ alice/helper/slack_helper.py
@@ -49,11 +49,11 @@ class SlackHelper(object):
                 print("Skipped: Posting in Slack because channel name was:", channel)
         except Exception as ex:
             msg = "<@UL91SP77H> Error in alice channel name %s"%channel
-            self.slack.chat.post_message(channel=ALICE_ERROR, text=msg, icon_url=self.icon, as_user=as_user, *args, **kwargs)
+            # self.slack.chat.post_message(channel=ALICE_ERROR, text=msg, icon_url=self.icon, as_user=as_user, *args, **kwargs)
             LOG.error("Error while posting alert to slack, please check if: \n1. The provided slack/hubot token for alice is correct " \
                   "and it has access to the channel=%s" \
                   "\n2.The channel name is correct\n" %(channel))
-            raise ex
+            # raise ex
 
     def directSlack(self, person, msg=None, as_user=False, *args, **kwargs):
         if self.config.is_debug:
