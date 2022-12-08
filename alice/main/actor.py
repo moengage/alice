e9aa07b932cae2c9c58ecf2480449400a014ffac
Habin Prasad <45557230+habin-163@users.noreply.github.com>
2022-12-08 19:36:31 +0530

    accepts multiple branches from config
    Updated actor.py
    accepts list of branch names
    Updated config to handle string values as well

--- alice/main/actor.py
+++ alice/main/actor.py
@@ -17,8 +17,9 @@ import time
 import pytz
 import shutil
 
+
 class Actor(Base):
-    
+
     def __init__(self, pr):
         self.pr = pr
         self.github = GithubHelper(self.pr)
@@ -33,7 +34,6 @@ class Actor(Base):
             self.merged_by = self.pr.config.getSlackName(self.pr.merged_by)
             self.save_data_for_later()
         self.sensitive_file_touched, self.change_requires_product_plus1 = self.parse_files_and_set_flags()
-
 
     def save_data_for_later(self):
         """
@@ -98,7 +98,7 @@ class Actor(Base):
                               % (self.pr.opened_by_slack, self.pr.repo))
                     bad_pr = False
                     break
-                LOG.info("***** review_comment %r" %review_comment)
+                LOG.info("***** review_comment %r" % review_comment)
                 created_by = self.pr.config.getSlackName(self.pr.opened_by)
                 if item["user"]["login"] != created_by and (review_comment.find("+1") != -1 or thumbsUpIcon):
                     LOG.debug("+1 is found from reviewer=%s marking No Alert " % item["user"]["login"])
@@ -141,16 +141,16 @@ class Actor(Base):
                 if not is_product_plus1:
                     bad_name_str = MSG_BAD_START + "@" + self.created_by
                     msg = MSG_NO_PRODUCT_REVIEW.format(name=bad_name_str, pr=self.pr.link_pretty, title=self.pr.title,
-                                                    branch=self.pr.base_branch, team=""
+                                                       branch=self.pr.base_branch, team=""
                                                        .join(self.pr.config.productTeamToBeNotified))
                     LOG.debug(msg)
                     self.slack.postToSlack(self.pr.config.alertChannelName, msg)
                     LOG.info("Bad PR={msg} repo:{repo}".format(repo=self.pr.repo, msg=self.is_bad_pr))
                     return {"msg": "Bad PR={msg} repo:{repo}".format(repo=self.pr.repo, msg=self.is_bad_pr)}
-                return {"msg":"Product approved so no alert, pr=%s" %self.pr.link_pretty}
-            return {"msg":"Skipped product review because no changes found which requires product +1 as well, PR=%s"
-                          %self.pr.link_pretty}
-        return {"msg": "Skipped product review because the PR=%s is not merged" %self.pr.link_pretty}
+                return {"msg": "Product approved so no alert, pr=%s" % self.pr.link_pretty}
+            return {"msg": "Skipped product review because no changes found which requires product +1 as well, PR=%s"
+                           % self.pr.link_pretty}
+        return {"msg": "Skipped product review because the PR=%s is not merged" % self.pr.link_pretty}
 
     def is_plus_1_in_comments(self, comments, team):
         """
@@ -175,7 +175,7 @@ class Actor(Base):
         """
         if self.pr.is_opened:
             if not self.pr.config.is_debug:
-                if self.pr.base_branch == self.pr.config.mainBranch:
+                if self.pr.base_branch in self.pr.config.mainBranch:
                     guideline_comment = SPECIAL_COMMENT
                     self.slack.postToSlack(self.pr.opened_by)
                 else:
@@ -186,7 +186,6 @@ class Actor(Base):
             return {"msg": "Skipped commenting because DEBUG is on "}
         return {"msg": "Skipped commenting because its not PR opened"}
 
-
     def notify_on_action(self):
         """
         notify respective folk(s) on particular action. Ex. on open notify to lead (or on merged, can configure that)
@@ -194,7 +193,7 @@ class Actor(Base):
         """
         desired_action = self.pr.config.actionToBeNotifiedFor
         if self.pr.action == desired_action:
-            if self.pr.base_branch == self.pr.config.mainBranch:
+            if self.pr.base_branch in self.pr.config.mainBranch:
                 msg = MSG_OPENED_TO_MAIN_BRANCH.format(repo=self.pr.repo, pr_by=self.created_by,
                                                        main_branch=self.pr.config.mainBranch, title=self.pr.title,
                                                        pr=self.pr.link_pretty, action=self.pr.action)
@@ -209,8 +208,7 @@ class Actor(Base):
                 self.slack.postToSlack(self.pr.config.personToBeNotified, msg)
                 LOG.info("Notified to %s on action %s" % (self.pr.config.personToBeNotified, self.pr.action))
                 return {"msg": "Notified to %s on action %s" % (self.pr.config.personToBeNotified, self.pr.action)}
-        return {"msg": "Skipped notify because its (%s) not desired event '%s'" %(self.pr.action, desired_action)}
-
+        return {"msg": "Skipped notify because its (%s) not desired event '%s'" % (self.pr.action, desired_action)}
 
     def remind_direct_release_guideline_on_merge(self):
         """
@@ -228,7 +226,6 @@ class Actor(Base):
             return {"msg": "skipped slack personally because not sensitive branch"}
         return {"msg": "skipped slack personally to %s because its not merge event" % self.created_by}
 
-
     def close_dangerous_pr(self):
         """
         close a Pull Request which is not supposed to be opened Ex. base=master head=feature
@@ -237,14 +234,14 @@ class Actor(Base):
         if self.pr.is_opened or self.pr.is_reopened:
             master_branch = self.pr.config.mainBranch
             qa_branch = self.pr.config.testBranch
-            if self.base_branch == master_branch and self.head_branch != qa_branch:
+            if self.base_branch in master_branch and self.head_branch != qa_branch:
                 msg = MSG_AUTO_CLOSE.format(tested_branch=qa_branch, main_branch=master_branch)
                 self.github.modify_pr(msg, "closed")
                 self.slack.postToSlack(self.pr.config.alertChannelName, "@" + self.created_by + ": " + msg)
                 LOG.info("closed dangerous PR %s" % self.pr.link_pretty)
                 return {"msg": "closed dangerous PR %s" % self.pr.link_pretty}
-            return {"msg": "skipped closing PR=%s because not raised to mainBranch %s" %(self.pr.link_pretty,
-                                                                                         master_branch)}
+            return {"msg": "skipped closing PR=%s because not raised to mainBranch %s" % (self.pr.link_pretty,
+                                                                                          master_branch)}
         return {"msg": "skipped closing PR because not a opened PR"}
 
     def notify_if_sensitive_modified(self):
@@ -260,33 +257,32 @@ class Actor(Base):
                 self.slack.postToSlack(self.pr.config.alertChannelName, msg)
                 LOG.info("informed %s because sensitive files are touched in pr=%s" %
                          (self.pr.config.devOpsTeamToBeNotified, self.pr.link_pretty))
-                return {"msg": "informed %s because sensitive files are touched" % self.pr.config.devOpsTeamToBeNotified}
+                return {
+                    "msg": "informed %s because sensitive files are touched" % self.pr.config.devOpsTeamToBeNotified}
             return {"msg": "Skipped sensitive files alerts because no sensitive file being touched"}
         return {
             "msg": "Skipped sensitive files alerts because its not PR merge event %s" %
                    self.pr.config.devOpsTeamToBeNotified}
-
 
     def notify_qa_sign_off(self):
         """
         Notify to respective folks when qa is passed and code is moved to main branch Eg. master
         :return:
         """
-        if self.pr.is_merged and self.pr.base_branch == self.pr.config.mainBranch \
+        if self.pr.is_merged and self.pr.base_branch in self.pr.config.mainBranch \
                 and self.pr.head_branch == self.pr.config.testBranch:
             msg = MSG_QA_SIGN_OFF.format(person=self.pr.config.personToBeNotified, pr=self.pr.link_pretty,
                                          dev_ops_team=self.pr.config.devOpsTeamToBeNotified,
                                          tech_team=self.pr.config.techLeadsToBeNotified)
 
             self.slack.postToSlack(self.pr.config.alertChannelName, msg,
-                               data=self.slack.getBot(self.pr.config.alertChannelName, self.merged_by))
+                                   data=self.slack.getBot(self.pr.config.alertChannelName, self.merged_by))
 
             """ for bot to keep data ready for future use"""
             write_to_file_from_top(self.pr.config.releaseFreezeDetailsPath, ":clubs:" +
                                    str(datetime.now(pytz.timezone('Asia/Calcutta')).strftime(
                                        '%B %d,%Y at %I.%M %p')) + " with <" + self.pr.link_pretty + "|master> code")
             clear_file(self.pr.config.codeFreezeDetailsPath)
-
 
     def notify_channel_on_merge(self):
         """
@@ -295,17 +291,16 @@ class Actor(Base):
         """
         if self.pr.is_merged:
             LOG.debug("**** Repo=%s, new merge came to=%s, setting trace to=%s channel"
-                      %(self.pr.repo, self.pr.base_branch, self.pr.config.codeChannelName))
+                      % (self.pr.repo, self.pr.base_branch, self.pr.config.codeChannelName))
             msg = MSG_CODE_CHANNEL.format(title=self.pr.title, desc=self.pr.description, pr=self.pr.link,
                                           head_branch=self.pr.head_branch, base_branch=self.pr.base_branch,
                                           pr_by=self.created_by, merge_by=self.merged_by)
             self.slack.postToSlack(self.pr.config.codeChannelName, msg)
             LOG.info("informed %s because pr=%s is merged into sensitive branch=%s" %
                      (self.pr.config.codeChannelName, self.pr.link_pretty, self.pr.base_branch))
-            return {"msg":"informed %s because pr=%s is merged into sensitive branch=%s" %
-                     (self.pr.config.codeChannelName, self.pr.link_pretty, self.pr.base_branch)}
-        return {"msg", "Skipped posting to code channel because '%s' is not merge event" %self.pr.action}
-
+            return {"msg": "informed %s because pr=%s is merged into sensitive branch=%s" %
+                           (self.pr.config.codeChannelName, self.pr.link_pretty, self.pr.base_branch)}
+        return {"msg", "Skipped posting to code channel because '%s' is not merge event" % self.pr.action}
 
     def notify_code_freeze(self):
         """
@@ -313,7 +308,7 @@ class Actor(Base):
         :return: relevant response dict
         """
         if self.pr.is_merged and (self.pr.base_branch == self.pr.config.testBranch \
-                and self.pr.head_branch == self.pr.config.devBranch):
+                                  and self.pr.head_branch == self.pr.config.devBranch):
             LOG.debug("*** PR merged from {dev_branch} to {qa_branch}, posting release items to slack".
                       format(dev_branch=self.pr.config.devBranch, qa_branch=self.pr.config.testBranch))
 
@@ -322,20 +317,20 @@ class Actor(Base):
                     '%B %d at %I.%M %p')) + " with <" + self.pr.link_pretty + "|PR>")
 
             try:
-                msg  = read_from_file(self.pr.config.releaseItemsFilePath)
+                msg = read_from_file(self.pr.config.releaseItemsFilePath)
                 LOG.debug("final msg =" + msg)
             except Exception, e:
                 return {"msg": "Skipped posting code-freeze because no details found in file %s"
                                % self.pr.config.releaseItemsFilePath}
 
-
             CODE_FREEZE_TEXT[0]["pretext"] = CODE_FREEZE_TEXT[0]["pretext"].format(dev_branch=self.pr.config.devBranch,
                                                                                    test_branch=self.pr.config.testBranch)
-            CODE_FREEZE_TEXT[0]["fields"][0]["title"] =  CODE_FREEZE_TEXT[0]["fields"][0].get("title")\
+            CODE_FREEZE_TEXT[0]["fields"][0]["title"] = CODE_FREEZE_TEXT[0]["fields"][0].get("title") \
                 .format(test_branch=self.pr.config.testBranch)
-            CODE_FREEZE_TEXT[0]["fields"][1]["value"] = CODE_FREEZE_TEXT[0]["fields"][1]["value"].format(pr=self.pr.link_pretty)
+            CODE_FREEZE_TEXT[0]["fields"][1]["value"] = CODE_FREEZE_TEXT[0]["fields"][1]["value"].format(
+                pr=self.pr.link_pretty)
             CODE_FREEZE_TEXT[0]["text"] = CODE_FREEZE_TEXT[0]["text"].format(msg=msg)
-            CODE_FREEZE_TEXT[0]["title_link"] = CODE_FREEZE_TEXT[0]["title_link"]\
+            CODE_FREEZE_TEXT[0]["title_link"] = CODE_FREEZE_TEXT[0]["title_link"] \
                 .format(release_notes_link=self.pr.config.release_notes_link)
 
             self.slack.postToSlack(channel=self.pr.config.alertChannelName, attachments=CODE_FREEZE_TEXT)
@@ -344,8 +339,8 @@ class Actor(Base):
                                                                      self.pr.link_pretty)}
         return {"msg", "Skipped posting to code-freeze because its not code-freeze event, {pr_action}ed from "
                        "{dev_branch} -> {qa_branch} ".format(pr_action=self.pr.action,
-                        dev_branch=self.pr.config.devBranch, qa_branch=self.pr.config.testBranch)}
-
+                                                             dev_branch=self.pr.config.devBranch,
+                                                             qa_branch=self.pr.config.testBranch)}
 
     def remind_all_finally_to_update_release_notes(self):
         """
@@ -361,7 +356,6 @@ class Actor(Base):
             self.slack.postToSlack(channel=self.pr.config.alertChannelName, msg=msg)
             self.clean_up_for_next_cycle()
 
-
     def clean_up_for_next_cycle(self):
         """ backup & clean-up file for next release """
         shutil.copy(self.pr.config.releaseItemsFilePath, self.pr.config.backupFilesPath + '_'
@@ -369,6 +363,3 @@ class Actor(Base):
         clear_file(self.pr.config.releaseItemsFileMergedBy)
         clear_file(self.pr.config.releaseItemsFilePath)  # clear file for next release content
         # NOTE: user has to manually delete data added when in debug mode
-
-
-
